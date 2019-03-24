978d8d7c768d3a441df0abd8094c5b0c294d276f
On Kilter <13891670+krisjecen@users.noreply.github.com>
2019-03-24 16:07:53 -0700

    added basic html structure, css, began js structure comments; added crystal and background image files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # unit-4-game
-Crystals Collector is a one-person puzzle game with numbers; it functions a bit like the card game blackjack
+Crystal Collector is a one-person puzzle game where you use randomly-valued crystals to try to reach the target number. Crystal Collector functions a bit like blackjack: there's a good bit of randomness, you can't always win, and if your score exceeds the target, you lose.
---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+// js for Crystal Collector game
+// will use jQuery
+
+// wait until the html is loaded
+$(document).ready(function() {
+    // initialize variables
+    var targetScore = null;
+    var userScore = null;
+    var c1 = null;
+    var c2 = null;
+    var c3 = null;
+    var c4 = null;
+    var userWins = 0;
+    var userLosses = 0;
+
+    // target the crystal area and set the images in place side by side
+    
+    
+    // generate random values for the four crystals
+    // on-click events for crystal buttons
+
+    // if userScore < targetScore, let buttons add value to userScore
+
+    // when a crystal is clicked, add its value to the userScore
+    // and update the userScore
+
+    // if userScore === targetScore, player wins
+    // increment userWins
+
+    // if userScore > targetScore, player loses
+    // increment userLosses
+
+    
+
+
+
+
+
+});
+
