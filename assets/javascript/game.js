79ba51f834517c1cdfd06ba7a789b6375bd22f51
On Kilter <13891670+krisjecen@users.noreply.github.com>
2019-03-24 23:43:33 -0700

    score updates, crystal values generated but not yet updating properly; css updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,31 +5,88 @@
 $(document).ready(function() {
     // initialize variables
     var targetScore = null;
-    var userScore = 69;
+    var userScore = 0;
     var c1 = null;
     var c2 = null;
     var c3 = null;
     var c4 = null;
     var userWins = 0;
     var userLosses = 0;
-    var x = null;
 
     // display the userScore on the screen
     $("#score").text(userScore);
+    $("#targetScore").text(targetScore);
+    $("#winsText").text(`Wins: ${userWins}`);
+    $("#lossesText").text(`Losses: ${userLosses}`);
 
     // target the crystal area and set the images in place side by side
     $("#c1").html("<img src='assets/images/crystal1.png' width='150px' class='border img-fluid'>");
     $("#c2").html("<img src='assets/images/crystal2.png' width='150px' class='border img-fluid'>");
     $("#c3").html("<img src='assets/images/crystal3.png' width='150px' class='border img-fluid'>");
     $("#c4").html("<img src='assets/images/crystal4.png' width='150px' class='border img-fluid'>");
+
+    // declare functions
+    //
+    // increase the userScore when a crystal is clicked
+    function newGame() {
+        // reset the userScore
+        userScore = 0;
+        $("#score").text(userScore);
+        // generate a new targetScore
+        targetScore = Math.floor(Math.random() * 102) + 19;
+        $("#targetScore").text(targetScore);
+        // generate new crystal values
+        c1 = Math.floor(Math.random() * 12) + 1;
+        c2 = Math.floor(Math.random() * 12) + 1;
+        c3 = Math.floor(Math.random() * 12) + 1;
+        c4 = Math.floor(Math.random() * 12) + 1;
+        console.log(c1);
+        console.log(c2);
+        console.log(c3);
+        console.log(c4);
+
+    }
     
-    // generate random values for the four crystals for the game (to be regenerated
-    // when the game restarts)
-    c1 = 6;
-    c2 = 5;
-    c3 = 2;
-    c4 = 10;
+    // need to update this value because c1 console logs accurately but crystalValue
+    // is still lingering from the first generated value
+    function addCrystalvalue(crystalValue) {
+        // takes the parameter data from the on-click event
+        console.log(c1);
+        userScore += crystalValue.data.crystal;
+        // update the score with the new value
+        $("#score").text(userScore);
+
+        if (userScore > targetScore) {
+            userLosses++;
+            $("#lossesText").text(`Losses: ${userLosses}`);
+            //start the next game
+            newGame()
+        }
+
+        // check to see if the player has won
+        else if (userScore === targetScore) {
+            userWins++;
+            $("#winsText").text(`Wins: ${userWins}`);
+            //start the next game
+            newGame()
+        }
+        
+    }
     
+    // generate a random target score for the game
+    //targetScore = Math.floor(Math.random() * 102) + 19;
+    //$("#targetScore").text(targetScore);
+
+
+    // generate random values for the four crystals for the game
+    // (to be regenerated when the game restarts)
+    // c1 = Math.floor(Math.random() * 12) + 1;
+    // c2 = Math.floor(Math.random() * 12) + 1;
+    // c3 = Math.floor(Math.random() * 12) + 1;
+    // c4 = Math.floor(Math.random() * 12) + 1;
+    
+
+    newGame()
 
     // on-click events for crystal buttons
     // when the crystal is clicked, pass the current value of the crystal into the addCrystalvalue fn
@@ -38,28 +95,11 @@ $(document).ready(function() {
     $("#c3").on("click", {crystal: c3}, addCrystalvalue);
     $("#c4").on("click", {crystal: c4}, addCrystalvalue);
 
-    //increase the userScore when a crystal is clicked
-    function addCrystalvalue(crystalValue) {
-        // takes the parameter data from the on-click event
-        userScore += crystalValue.data.crystal;
-        console.log(userScore);
-        $("#score").text(userScore);
-    }
+    
 
 
-    // function addCrystalvalue(crystalValue) {
-    //     userScore += crystalValue;
-    //     console.log(userScore);
-    //     $("#score").text(userScore);
-    // }
-
+    
     // if userScore < targetScore, let buttons add value to userScore
-
-    // when a crystal is clicked, add its value to the userScore
-    // and update the userScore
-
-    // if userScore === targetScore, player wins
-    // increment userWins
 
     // if userScore > targetScore, player loses
     // increment userLosses
